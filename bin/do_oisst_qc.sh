55a245bbf2d74d1f200249f20028628c7ad409b9
Seth Underwood <Seth.Underwood@noaa.gov>
2016-10-12 14:44:25 -0400

    Use if/else/fi to protect section
    used if/else/fi to protect code that shouldn't be run if the *.OK file
    found.
    
    Updated .gitignore to ignore log directory

--- bin/do_oisst_qc.sh
+++ bin/do_oisst_qc.sh
@@ -7,6 +7,11 @@ echoerr() {
 # Set the umask for world readable
 umask 022
 
+# Check limits
+#ulimit -S -s unlimited
+#ulimit -H -s unlimited
+ulimit -a
+
 # Location of this script
 BIN_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
@@ -85,95 +90,95 @@ OUTFILE=${OUT_DIR}/${sst_cm2}
 # Check of existance of the output file.  If it exists, exit
 if [[ -e ${OUTFILE}.OK ]]; then
     echoerr "File '${OUTFILE}' already exists.  Not processing."
-    exit 0
-fi
-
-# Get a list of files, and verify that all days in the range are present
-# $( date -d "2016-$m-01 + 1month - 1day" '+%d' ) get number of days in month
-# inFiles is a list of files to process
-inFiles=''
-
-for m in $( seq -f '%02g' 1 $monPrev ); do
-    daysInMonth=$( date -d "${yearPrev}-${m}-01 + 1month - 1day" '+%d' )
-
-    for d in $( seq -f '%02g' 1 $daysInMonth ); do
-	f_base=${RAW_DIR}/${yearPrev}/avhrr-only-v2.${yearPrev}${m}${d}
-	# Do we need to use the preliminary data?
-	if [ -e ${f_base}.nc ]; then
-	    inFiles="${inFiles} ${f_base}.nc"
-	elif [ -e ${f_base}_preliminary.nc ]; then
-	    inFiles="${inFiles} ${f_base}_preliminary.nc"
-	else
-	    echoerr "ERROR: Unable to find raw data file file for ${yearPrev}-${m}-${d}"
-	    exit 1
-	fi
-    done
-done
-
-# Check for the existance of ${yearCur}-${monCur}-01
-f_base=${RAW_DIR}/${yearCur}/avhrr-only-v2.${yearCur}${monCur}01
-if [ -e ${f_base}.nc ]; then
-    inFiles="${inFiles} ${f_base}.nc"
-elif [ -e ${f_base}_preliminary.nc ]; then
-    inFiles="${inFiles} ${f_base}_preliminary.nc"
 else
-    echoerr "ERROR: Unable to find raw data file file for ${yearCur}-${m}-01"
-    exit 1
-fi
+    # Get a list of files, and verify that all days in the range are present
+    # $( date -d "2016-$m-01 + 1month - 1day" '+%d' ) get number of days in month
+    # inFiles is a list of files to process
+    inFiles=''
 
-# Combine the daily files into a single file
-# Keep a history of which input files were used in the $OUTFILE.log file
-if [[ -e ${OUTFILE}.log ]]; then
-    rm -f ${OUTFILE}.log
-fi
+    for m in $( seq -f '%02g' 1 $monPrev ); do
+        daysInMonth=$( date -d "${yearPrev}-${m}-01 + 1month - 1day" '+%d' )
 
-# Output file for this step:
-sst_mean="sst.day.mean${year}.v2.nc"
+        for d in $( seq -f '%02g' 1 $daysInMonth ); do
+            f_base=${RAW_DIR}/${yearPrev}/avhrr-only-v2.${yearPrev}${m}${d}
+            # Do we need to use the preliminary data?
+            if [ -e ${f_base}.nc ]; then
+                inFiles="${inFiles} ${f_base}.nc"
+            elif [ -e ${f_base}_preliminary.nc ]; then
+                inFiles="${inFiles} ${f_base}_preliminary.nc"
+            else
+                echoerr "ERROR: Unable to find raw data file file for ${yearPrev}-${m}-${d}"
+                exit 1
+            fi
+        done
+    done
 
-for f in $inFiles; do
-    ferret <<EOF
+    # Check for the existance of ${yearCur}-${monCur}-01
+    f_base=${RAW_DIR}/${yearCur}/avhrr-only-v2.${yearCur}${monCur}01
+    if [ -e ${f_base}.nc ]; then
+        inFiles="${inFiles} ${f_base}.nc"
+    elif [ -e ${f_base}_preliminary.nc ]; then
+        inFiles="${inFiles} ${f_base}_preliminary.nc"
+    else
+        echoerr "ERROR: Unable to find raw data file file for ${yearCur}-${monCur}-01"
+        exit 1
+    fi
+
+    # Combine the daily files into a single file
+    # Keep a history of which input files were used in the $OUTFILE.log file
+    if [[ -e ${OUTFILE}.log ]]; then
+        rm -f ${OUTFILE}.log
+    fi
+
+    # Output file for this step:
+    sst_mean="sst.day.mean${year}.v2.nc"
+
+    for f in $inFiles; do
+        ferret <<EOF
+set memory/size=1000
 use "${f}"
 save/app/file="${sst_mean}" sst
 exit
 EOF
 
-    echo ${f} >> ${OUTFILE}.log
-    # Remove the ferret file
-    rm -f ferret*.jnl*
-done
+        echo ${f} >> ${OUTFILE}.log
+        # Remove the ferret file
+        rm -f ferret*.jnl*
+    done
 
-# Regrid the data for use with CM2 models
-ferret <<EOF
-set memory/size=1280
+    # Regrid the data for use with CM2 models
+    ferret <<EOF
+set memory/size=1000
 use "${DATA_DIR}/grid_spec.nc"
 use "${sst_mean}"
 let temp = sst[gx=GEOLON_T[d=1],gy=GEOLAT_T[d=1],t=${sDate}:${eDate}]
 save/clobber/file=tmp1.nc temp
 exit
 EOF
-rm -f ferret*.jnl*
+    rm -f ferret*.jnl*
 
-# Rename variables
-ncrename -v TEMP,temp tmp1.nc
-ncrename -v TIME,t tmp1.nc
-ncrename -v GRIDLON_T,gridlon_t tmp1.nc
-ncrename -v GRIDLAT_T,gridlat_t tmp1.nc
-ncrename -d TIME,t tmp1.nc
-ncrename -d GRIDLON_T,gridlon_t tmp1.nc
-ncrename -d GRIDLAT_T,gridlat_t tmp1.nc
+    # Rename variables
+    ncrename -v TEMP,temp tmp1.nc
+    ncrename -v TIME,t tmp1.nc
+    ncrename -v GRIDLON_T,gridlon_t tmp1.nc
+    ncrename -v GRIDLAT_T,gridlat_t tmp1.nc
+    ncrename -d TIME,t tmp1.nc
+    ncrename -d GRIDLON_T,gridlon_t tmp1.nc
+    ncrename -d GRIDLAT_T,gridlat_t tmp1.nc
 
-# average away the z-axis in file 2 and removing the ZLEV variable
-ncwa -O -h -a ZLEV tmp1.nc tmp2.nc
-ncrcat -O -h -x -v ZLEV tmp2.nc ${sst_cm2}
+    # average away the z-axis in file 2 and removing the ZLEV variable
+    ncwa -O -h -a ZLEV tmp1.nc tmp2.nc
+    ncrcat -O -h -x -v ZLEV tmp2.nc ${sst_cm2}
 
-# Save file
-cp ${sst_cm2} ${OUTFILE}
-touch ${OUTFILE}.OK
+    # Save file
+    cp ${sst_cm2} ${OUTFILE}
+    touch ${OUTFILE}.OK
+fi
 
 # Copy file to remote site
 if [[ ! -z ${XFER_TARGET} ]]; then
     for f in ${OUTFILE} ${OUTFILE}.OK ${OUTFILE}.log
     do
-	$XFER_COMMAND ${f} ${XFER_TARGET}
+        $XFER_COMMAND ${f} ${XFER_TARGET}
     done
 fi
